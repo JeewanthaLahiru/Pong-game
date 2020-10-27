bdf0fe0f173c8193869d2b03ae379fc5e690985f
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-10-27 23:36:53 +0530

    added movement to left paddle

--- main.lua
+++ main.lua
@@ -6,9 +6,14 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 function love.load()
     love.graphics.setDefaultFilter('nearest','nearest')
     smallFont = love.graphics.newFont('font.ttf', 8)
+
+    scoreFont = love.graphics.newFont('font.ttf', 32)
+
     love.graphics.setFont(smallFont)
 
     push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT,{
@@ -16,6 +21,9 @@ function love.load()
         resizable = false,
         vsync = true
     })
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
 end
 
 function love.keypressed(key)
@@ -24,16 +32,28 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+    if love.keyboard.isDown('w') then 
+        player1Y = player1Y + -PADDLE_SPEED*dt
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED*dt
+    end
+end
+
+
 function love.draw()
     push:apply('start')
     love.graphics.clear(40/255,45/255,52/255,255/255)
 
     love.graphics.printf('Hello Pong',0,20,VIRTUAL_WIDTH,'center')
 
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH-10, VIRTUAL_HEIGHT-50, 5, 20)
+    --first paddle(left)
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
+    --second paddle
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH-10, player2Y, 5, 20)
 
     love.graphics.rectangle('fill', VIRTUAL_WIDTH/2-2, VIRTUAL_HEIGHT/2-2, 4, 4)
 
     push:apply('end')
 end
+
